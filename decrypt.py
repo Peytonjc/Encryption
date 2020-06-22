3be9a758821b503ee5c6894e46a77f4deb3da408
Peytonjc <35534934+Peytonjc@users.noreply.github.com>
2020-06-22 15:40:45 -0500

    Encrypting and Decrypting messages. Must know the password that was used.

---
+++ decrypt.py
@@ -0,0 +1,110 @@
+# Opening and reading the input file
+file = input("What is the name of the file you would like to decrypt:")
+inFile = open(file, "r")
+inText = inFile.read()
+inFile.close()
+print(inText)
+
+padNum = 5 - (len(inText) % 5)
+if padNum == 5:
+    padNum = 0
+for i in range(padNum):
+    inText = inText + ' '
+textMat = []
+i = 0
+for j in range(int(len(inText) / 5)):
+    a = []
+    for k in range(5):
+        a.append(inText[i])
+        i = i + 1
+    textMat.append(a)
+
+# Ask for password
+pas = input("Input a four digit password:")
+
+pasOne = int(pas[1])
+pasTwo = int(pas[3])
+pasThree = int(pas[0])
+pasFour = int(pas[2])
+
+newMat = textMat
+rowNum = int(len(inText) / 5)
+
+# Decryption of part 5
+outMat = []
+for j in range(3):
+    if j != 0:
+        newMat = outMat
+        outMat = []
+    for i in range(rowNum):
+        if i >= (rowNum - 1):
+            a = textMat[i][:4] + list(newMat[i - (rowNum+1)][4])
+        else:
+            a = textMat[i][:4] + list(newMat[i - 1][4])
+        outMat.append(a)
+newMat = outMat
+outMat = []
+
+# Decryption of part 4
+for j in range(pasFour):
+    if j != 0:
+        newMat = outMat
+        outMat = []
+    for i in range(rowNum):
+        if i >= (rowNum - 1):
+            a = textMat[i][:3] + list(newMat[i - (rowNum+1)][3]) + list(newMat[i][4])
+        else:
+            a = textMat[i][:3] + list(newMat[i - 1][3]) + list(newMat[i][4])
+        outMat.append(a)
+newMat = outMat
+outMat = []
+
+# Decryption of part 3
+for j in range(pasThree):
+    if j != 0:
+        newMat = outMat
+        outMat = []
+    for i in range(rowNum):
+        if i >= (rowNum - 1):
+            a = textMat[i][:2] + list(newMat[i - (rowNum+1)][2]) + newMat[i][3:]
+        else:
+            a = textMat[i][:2] + list(newMat[i - 1][2]) + newMat[i][3:]
+        outMat.append(a)
+newMat = outMat
+outMat = []
+
+# Decryption of part 2
+for j in range(pasTwo):
+    if j != 0:
+        newMat = outMat
+        outMat = []
+    for i in range(rowNum):
+        if i >= (rowNum - 1):
+            a = list(textMat[i][0] + newMat[i - (rowNum+1)][1]) + newMat[i][2:]
+        else:
+            a = list(textMat[i][0] + newMat[i - 1][1]) + newMat[i][2:]
+        outMat.append(a)
+newMat = outMat
+outMat = []
+
+# Decryption of part 1
+for j in range(pasOne):
+    if j != 0:
+        newMat = outMat
+        outMat = []
+    for i in range(rowNum):
+        if i >= (rowNum - 1):
+            a = list(newMat[i - (rowNum+1)][0]) + newMat[i][1:]
+        else:
+            a = list(newMat[i - 1][0]) + newMat[i][1:]
+        outMat.append(a)
+print('\n')
+for row in outMat:
+    print(row)
+
+outString = []
+for row in outMat:
+    outString.extend(row)
+outString = "".join(outString)
+outFile = open("output.txt", "w")
+outFile.write(outString)
---
+++ encrypt.py
@@ -0,0 +1,113 @@
+# Opening and reading the input file
+file = input("What is the name of the file you would like to encrypt:")
+inFile = open(file, "r")
+inText = inFile.read()
+inFile.close()
+print(inText)
+
+# Add padding to the text file in the form of whitespace and convert to a matrix format
+padNum = 5 - (len(inText) % 5)
+if padNum == 5:
+    padNum = 0
+for i in range(padNum):
+    inText = inText + ' '
+textMat = []
+i = 0
+for j in range(int(len(inText) / 5)):
+    a = []
+    for k in range(5):
+        a.append(inText[i])
+        i = i + 1
+    textMat.append(a)
+
+# Ask for password
+pas = input("Input a four digit password:")
+
+newMat = []
+rowNum = int(len(inText) / 5)
+
+# Modify matrix based on password (part 1)
+pasOne = int(pas[1])
+for j in range(pasOne):
+    if j != 0:
+        textMat = newMat
+        newMat = []
+    for i in range(rowNum):
+        if i >= (rowNum - 1):
+            a = list(textMat[i - (rowNum-1)][0]) + textMat[i][1:]
+        else:
+            a = list(textMat[i + 1][0]) + textMat[i][1:]
+        newMat.append(a)
+
+textMat = newMat
+newMat = []
+
+# Modify matrix based on password (part 2)
+pasTwo = int(pas[3])
+for j in range(pasTwo):
+    if j != 0:
+        textMat = newMat
+        newMat = []
+    for i in range(rowNum):
+        if i >= (rowNum - 1):
+            a = list(textMat[i][0] + textMat[i - (rowNum-1)][1]) + textMat[i][2:]
+        else:
+            a = list(textMat[i][0] + textMat[i + 1][1]) + textMat[i][2:]
+        newMat.append(a)
+
+textMat = newMat
+newMat = []
+
+# Modify matrix based on password (part 3)
+pasThree = int(pas[0])
+for j in range(pasThree):
+    if j != 0:
+        textMat = newMat
+        newMat = []
+    for i in range(rowNum):
+        if i >= (rowNum - 1):
+            a = textMat[i][:2] + list(textMat[i - (rowNum-1)][2]) + textMat[i][3:]
+        else:
+            a = textMat[i][:2] + list(textMat[i + 1][2]) + textMat[i][3:]
+        newMat.append(a)
+
+textMat = newMat
+newMat = []
+
+# Modify matrix based on password (part 4)
+pasFour = int(pas[2])
+for j in range(pasFour):
+    if j != 0:
+        textMat = newMat
+        newMat = []
+    for i in range(rowNum):
+        if i >= (rowNum - 1):
+            a = textMat[i][:3] + list(textMat[i - (rowNum-1)][3]) + list(textMat[i][4])
+        else:
+            a = textMat[i][:3] + list(textMat[i + 1][3]) + list(textMat[i][4])
+        newMat.append(a)
+
+textMat = newMat
+newMat = []
+
+# Modify matrix based on coded standard (part 5)
+for j in range(3):
+    if j != 0:
+        textMat = newMat
+        newMat = []
+    for i in range(rowNum):
+        if i >= (rowNum - 1):
+            a = textMat[i][:4] + list(textMat[i - (rowNum-1)][4])
+        else:
+            a = textMat[i][:4] + list(textMat[i + 1][4])
+        newMat.append(a)
+for row in newMat:
+    print(row)
+
+# Combine results in a string and write to file
+outString = []
+for row in newMat:
+    outString.extend(row)
+outString = "".join(outString)
+outFile = open("output.txt", "w")
+outFile.write(outString)
